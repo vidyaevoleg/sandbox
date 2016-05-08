fe7d1be24ad2eb1d168edf87967ca294f0525f56
vidyaevoleg <olezhka-vidyayev@mail.ru>
2016-05-08 21:37:57 +0300

    1 chapter

---
+++ app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var static = require('node-static');
+const PORT=8080; 
+
+var file = new static.Server('./public');
+ 
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
+
